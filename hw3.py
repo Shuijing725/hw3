bebbf1951637af9d450088007b49536ba51cceb1
DESKTOP-P975C2P\Shuijing <eminnie725@outlook.com>
2017-07-06 22:32:21 -0500

    hw3 output format adjusted

--- hw3.py
+++ hw3.py
@@ -1,4 +1,4 @@
-import numpy as np
+# import numpy as np
 import sys
 import copy
 import itertools
@@ -9,6 +9,8 @@ class Apriori(object):
 		self.min_support = 1 # default min_support is 1
 		self.itemsets = [] # list of tuples, where tuples are (itemset, support), itemset is the set of frequent patterns of size k
 		self.k = 0  # index of itemset we are working with now ("actual k" = self.k + 1)
+		self.closet = [] # list of close sets, structure same as self.itemsets
+		self.maxset = [] # list of max sets, structure same as self.itemsets
 		# self.output_set = []
 
 	# read input from std and store self.db and self.min_support
@@ -86,7 +88,7 @@ class Apriori(object):
 			self.itemsets.append(fk)	
 			# print(self.itemsets)
 			
-		return self.itemsets
+		# return self.itemsets
 
 
 	####################### suppose we have updated k!!!!!!!!
@@ -145,28 +147,154 @@ class Apriori(object):
 		self.itemsets[self.k].sort()
 		# print(self.itemsets[self.k - 1])
 
-	# print out the result in required format
-	def print_output(self):
-		for itemlist in self.itemsets:
-			if itemlist:
-				# print(itemlist)
-				for key, value in itemlist:
-					s = " ".join(list(key))
-					# print(s)
-					# print(s[1:])
-					# print(value, ' [', s, ']')
-					print(str(value) + ' [' + str(s) + ']')
-			# output = list(set(itemlist))
-			# print(output)
+	# algorithm to mine the clsoed frequent patterns from the db
+	def mine_closet(self):
+		# for frequent itemsets of all sizes
+		for i in range(0, len(self.itemsets) - 1): # should not reach the empty set in self.itemsets
+			close = []
+			# j = (set(pattern), count)
+			# for all frequent items of size i
+			for kj, vj in self.itemsets[i]:
+				# print("")
+				# print("kj = ", kj, "vj = ", vj)
+				# is_close = True
+				# for kk, vk in self.itemsets[i+1]:
+				# 	print("kk = ", kk, "vk = ", vk)
+				# 	if (kj.issubset(kk)) and (vj <= vk):
+				# 		is_close = False
+				# if for all frequent itemsets one size bigger, they are either not subset of kj 
+				# or count(aka, vj) is larger than the bigger frequent itemset, it is closed
+				if all(((not kj.issubset(kk)) or (vj > vk)) for kk, vk in self.itemsets[i+1]):
+				# if is_close: 
+				# 	print("is_close = True")
+					close.append((kj, vj)) 
+				# for kk, vk in range(len(self.itemsets[i+1])):
+				# 	if (kj not in kk) or (vj > vk):
+			# print(close)
+			self.closet.append(close)
 
+	# algorithm to mine the clsoed frequent patterns from the db
+	def mine_maxset(self):
+		for i in range(0, len(self.itemsets) - 1): # should not reach the empty set in self.itemsets
+			max_temp = []
+			# for all frequent items of size i
+			for kj, vj in self.itemsets[i]:
+				# if for all frequent itemsets one size bigger, they are either not subset of kj 
+				# it is a max pattern
+				if all((not kj.issubset(kk)) for kk, vk in self.itemsets[i+1]):
+					max_temp.append((kj, vj)) 
+				
+			# print(close)
+			self.maxset.append(max_temp)
+
+
+
+	# print out the result (apriori: 1; closed set: 2; max set: 3) in required format
+	def print_output(self, list_to_print):
+		if list_to_print == 1: 
+			flat_list = []
+			# flatten all frequent patterns (self.itemsets)
+			for itemlist in self.itemsets:
+				if itemlist:
+					for key, val in itemlist:
+						l = list(key)
+						l.sort()
+						flat_list.append((l, val))
+			# print(flat_list)
+			# sort the falttened list (set(pattern), count)
+			flat_list = [(list(key), val) for key, val in flat_list] # covert set(pattern) to list
+			flat_list.sort(key = lambda x: x[0]) # sort by pattern
+			flat_list.sort(key = lambda x: x[1], reverse = True) #sort by count in descending order
+			for key, value in flat_list:
+				# key.sort()
+				# print('l = ', l)
+				s = " ".join(key)
+				# print(s)
+				# print(s[1:])
+				# print(value, ' [', s, ']')
+				print(str(value) + ' [' + str(s) + ']')
+			# for itemlist in flat_list:
+			# 	temp_list = [(list(key), val) for key, val in itemlist]
+			# 	temp_list.sort(key = lambda x: x[0])
+			# 	print('199:', temp_list)
+			# 	temp_list.sort(key = lambda x: x[1], reverse = True)
+			# 	print('201:', temp_list)
+			# 	for key, value in temp_list:
+			# 		key.sort()
+			# 		# print('l = ', l)
+			# 		s = " ".join(key)
+			# 		# print(s)
+			# 		# print(s[1:])
+			# 		# print(value, ' [', s, ']')
+			# 		print(str(value) + ' [' + str(s) + ']')
+
+		elif list_to_print == 2:
+			flat_list = []
+			# flatten all frequent patterns (self.itemsets)
+			for itemlist in self.closet:
+				if itemlist:
+					for key, val in itemlist:
+						l = list(key)
+						l.sort()
+						flat_list.append((l, val))
+			# print(flat_list)
+			# sort the falttened list
+			flat_list = [(list(key), val) for key, val in flat_list]
+			flat_list.sort(key = lambda x: x[0])
+			flat_list.sort(key = lambda x: x[1], reverse = True)
+			for key, value in flat_list:
+				# key.sort()
+				# print('l = ', l)
+				s = " ".join(key)
+				# print(s)
+				# print(s[1:])
+				# print(value, ' [', s, ']')
+				print(str(value) + ' [' + str(s) + ']')
+
+		elif list_to_print == 3:
+			flat_list = []
+			# flatten all frequent patterns (self.itemsets)
+			for itemlist in self.maxset:
+				if itemlist:
+					for key, val in itemlist:
+						l = list(key)
+						l.sort()
+						flat_list.append((l, val))
+			# print(flat_list)
+			# sort the falttened list
+			flat_list = [(list(key), val) for key, val in flat_list]
+			flat_list.sort(key = lambda x: x[0])
+			flat_list.sort(key = lambda x: x[1], reverse = True)
+			for key, value in flat_list:
+				# key.sort()
+				# print('l = ', l)
+				s = " ".join(key)
+				# print(s)
+				# print(s[1:])
+				# print(value, ' [', s, ']')
+				print(str(value) + ' [' + str(s) + ']')
+			
+		else:
+			return
+			
 
 
 
 def main():
+	# apriori 
 	a = Apriori()
 	a.process_input()
-	print(a.apriori())
-	a.print_output()
+	a.apriori()
+	a.print_output(1)
+	# closed sets
+	a.mine_closet()
+	print("")
+	a.print_output(2)
+	# max sets
+	print("")
+	a.mine_maxset()
+	a.print_output(3)
+
 
 if __name__ == "__main__": 
 	main()
